8147d5422a75389e37f6f5143fdbec92890ce7b3
manishekaneja <manishekaneja@gmail.com>
2017-07-12 14:25:33 +0530

    index2 added

---
+++ public/assests/script/js.js
@@ -0,0 +1,53 @@
+        $(document).ready(function () {
+            /* setInterval(function () {
+                 $("#rated").empty().html($("#rating").val());
+             }, 4);*/
+            $("#search").click(function () {
+                var mname = add($("#mname").val());
+                var url = 'http://www.omdbapi.com/?apikey=ec6483bd&s=' + mname;
+                console.log("called ", url);
+                $.get(url).done(function (data) {
+                    document.getElementById("searchR").style.display = "block";
+                    var str = "";
+                    if (data.Response === 'False') {
+                        console.log(data.Response);
+                        str += "<h1>Item not found</h1>";
+                    } else {
+                        //console.log(data);
+                        for (var i = 0; i < data.Search.length; i++) {
+                            str += "<div class=\"outline\" onclick=\"getmovie('" + data.Search[i].Title + "')\"style=\"display:inline-flex; width:90%;\"> <img src=\"" + data.Search[i].Poster + "\" alt=\"\"> <div style=\"padding:1rem;\"><span name=\"title\">" + data.Search[i].Title + "</span><span name=\"year\">" + data.Search[i].Year + " </span><span name=\"type\">" + firstCap(data.Search[i].Type) + "</span> </div> </div>";
+
+                        }
+                        console.log(data.Response);
+                    }
+                    $("#searchR").empty().html(str);
+                    console.log(data);
+                }).fail(function (error) {
+                    console.log("ajax result", error);
+                });
+            });
+            $('#mname').keyup(function () {
+                if (event.keyCode == 13) {
+                    $('#search').click();
+                } else {
+                    return false
+                }
+            });
+        });
+
+        function getmovie(e) {
+            console.log(e);
+        }
+
+        function add(mname) {
+            for (i = 0; i < mname.length; i++) {
+                if (mname[i] == ' ') {
+                    mname = mname.replace(' ', '+');
+                }
+            }
+            return mname;
+        }
+
+        function firstCap(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        }
